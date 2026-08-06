4a041801fb7e6bdc9a81e093bf22c01a4af37864
LushnikovSR <lust48h@gmail.com>
2026-08-06 21:15:14 +0300

    Fixed task test-integration in Taskfile

--- order/internal/service/order/suite_test.go
+++ order/internal/service/order/suite_test.go
@@ -2,12 +2,18 @@ package order
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/suite"
 
 	grpcMocks "github.com/LushnikovSR/spaceship_factory/order/internal/client/grpc/mocks"
 	"github.com/LushnikovSR/spaceship_factory/order/internal/repository/mocks"
+	"github.com/LushnikovSR/spaceship_factory/platform/pkg/logger"
+)
+
+var (
+	loggerLevelValue = "debug"
 )
 
 type ServiceSuite struct {
@@ -25,6 +31,11 @@ func (s *ServiceSuite) SetupTest() {
 	s.inventoryClient = grpcMocks.NewInventoryClient(s.T())
 	s.paymentClient = grpcMocks.NewPaymentClient(s.T())
 	s.service = NewService(s.orderRepository, s.inventoryClient, s.paymentClient)
+
+	err := logger.Init(loggerLevelValue, true)
+	if err != nil {
+		panic(fmt.Errorf("не удалось инициализировать логгер: %w", err))
+	}
 }
 
 func (s *ServiceSuite) TearDownTest() {}
